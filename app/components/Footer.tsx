fab48677a8707d11aeea3d7d665f74daead04bca
apple2447 <vookword@gmail.com>
2026-06-04 10:31:52 +0700

    “update”

---
+++ app/components/Footer.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+export default function Footer() {
+    return(
+    <footer className="bg-gray-800 text-white p-4 wt-8">
+        <p>&copy; 2026 โดย กรภัทร์ TumSab. copyright correct</p>
+    </footer>
+    )
+}
